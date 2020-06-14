3452d11e00ad21277813033122132ec9c9b6ead3
liviu <liviu.moraru@gmail.com>
2020-06-14 13:08:06 +0300

    First commit - Singleton. All projects. Start MyCode
    First commit - Singleton. All projects. Start MyCode

---
+++ Singleton/MySingleton/MySingleton/v1_Naive/Singleton.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace MySingleton.v1_Naive
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            
+        }
+    }
+}
