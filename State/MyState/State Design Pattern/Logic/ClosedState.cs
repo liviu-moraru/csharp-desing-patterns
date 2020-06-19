fb6a1f178cdd1ea5801e38c1c262855e393255bc
liviu_moraru <Liviu_Moraru@Dellteam.com>
2020-06-19 12:36:44 +0300

    Concrete States

---
+++ State/MyState/State Design Pattern/Logic/ClosedState.cs
@@ -0,0 +1,25 @@
+namespace State_Design_Pattern.Logic
+{
+    public class ClosedState : BookingState
+    {
+        public override void EnterState(BookingContext booking)
+        {
+            
+        }
+
+        public override void Cancel(BookingContext booking)
+        {
+            
+        }
+
+        public override void DataPassed(BookingContext booking)
+        {
+            
+        }
+
+        public override void EnterDetails(BookingContext booking, string attendee, int ticketCount)
+        {
+            
+        }
+    }
+}
