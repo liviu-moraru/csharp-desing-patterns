ee8bfe9f42f4d4184699f86165572fc15a62a576
liviu_moraru <Liviu_Moraru@Dellteam.com>
2020-06-19 10:42:55 +0300

    The Abstract State

---
+++ State/MyState/State Design Pattern/Logic/BookingState.cs
@@ -0,0 +1,10 @@
+namespace State_Design_Pattern.Logic
+{
+    public abstract class BookingState
+    {
+        public abstract void EnterState(BookingContext booking);
+        public abstract void Cancel(BookingContext booking);
+        public abstract void DataPassed(BookingContext booking);
+        public abstract void EnterDetails(BookingContext booking, string attendee, int ticketCount);
+    }
+}
