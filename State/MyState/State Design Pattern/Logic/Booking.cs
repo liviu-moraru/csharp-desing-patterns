fe379dd1230886a8201971e55d6b1972093a7d15
liviu_moraru <Liviu_Moraru@Dellteam.com>
2020-06-19 10:29:04 +0300

    Complete the Naive Approach

--- State/MyState/State Design Pattern/Logic/Booking.cs
+++ State/MyState/State Design Pattern/Logic/Booking.cs
@@ -16,29 +16,69 @@ namespace State_Design_Pattern.Logic
         public int BookingID { get; set; }
 
         private CancellationTokenSource cancelToken;
+        private bool isNew;
+        private bool isPending;
        
         public Booking(MainWindow view)
         {
+            isNew = true;
             View = view;
+            BookingID = new Random().Next();
+            ShowState("New");
+            View.ShowEntryPage();
         }
 
         public void SubmitDetails(string attendee, int ticketCount)
         {
-           
+            if (isNew)
+            {
+                isNew = false;
+                isPending = true;
+                Attendee = attendee;
+                TicketCount = ticketCount;
+                cancelToken = new CancellationTokenSource();
+                StaticFunctions.ProcessBooking(this, ProcessingComplete, cancelToken);
+                View.ShowStatusPage("Processing");
+            }
         }
 
         public void Cancel()
         {
-           
+            if (isNew)
+            {
+                ShowState("Closed");
+                View.ShowStatusPage("Canceled by user");
+                isNew = false;
+            }
+            else if (isPending == true)
+            {
+                cancelToken.Cancel();
+            }
+            else
+            {
+                View.ShowError("Closed bookings cannot be cancelled");
+            }
+            
         }
 
         public void DatePassed()
         {
-
+            if (isNew)
+            {
+                ShowState("Closed");
+                View.ShowStatusPage("Booking expired");
+                isNew = false;
+            }
+            else
+            {
+                View.ShowError("Closed bookings cannot be cancelled");
+            }
+            
         }
 
         public void ProcessingComplete(Booking booking, ProcessingResult result)
         {
+            isPending = false;
             switch (result)
             {
                 case ProcessingResult.Sucess:
@@ -49,6 +89,7 @@ namespace State_Design_Pattern.Logic
                     View.ShowProcessingError();
                     Attendee = string.Empty;
                     BookingID = new Random().Next();
+                    isNew = true;
                     ShowState("New");
                     View.ShowEntryPage();
                     break;
