7cd901417e4659bb10223e7c894b809960ab9f3e
Liviu Moraru <liviu.moraru@gmail.com>
2020-06-22 17:13:22 +0300

    Initial project

---
+++ Strategy/MyStrategy/Strategy Pattern First Look/Strategy Pattern First Look/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Strategy_Pattern_First_Look.Business.Models;
+
+namespace Strategy_Pattern_First_Look
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails
+                {
+                    OriginCountry = "Sweden",
+                    DestinationCountry = "Swedem"
+                }
+            };
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+            order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+            Console.WriteLine(order.GetTax());
+        }
+    }
+}
